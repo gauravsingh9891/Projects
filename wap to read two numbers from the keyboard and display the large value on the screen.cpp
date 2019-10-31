9d42bbc333ee16aac7e68e5dc3abb67c8ee33e45
gauravsingh9891 <gauravhacker8800@gmail.com>
2019-10-30 23:56:06 -0700

    Pg 28 qns = 2.2

---
+++ wap to read two numbers from the keyboard and display the large value on the screen.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{
+	int a,b;
+	cout<<"\n Enter the two number = ";
+	cin>>a>>b;
+	if(a>b)
+	{
+		cout<<a<<" is greatest number ";
+	}
+	else
+	{
+		cout<<b<<" is greatest number ";
+	}
+	getch();
+}
