30ac026bdf112cea3062ff67bbb453e385e272a0
gauravsingh9891 <gauravhacker8800@gmail.com>
2019-10-31 00:12:12 -0700

    Pg 28 qns = 2.3

---
+++ wap that inputs a character from keyboard and display its corresponding ASCII value on the screen.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{
+	int a;
+	char ch;
+	a=ch;
+	cout<<"\n Enter the character = ";
+	cin>>ch;
+	printf("ACSII value of %c is %d",ch,ch);  
+	getch();
+}
