bdb19c877684aeb3008b4ec1b141ce1b7be43659
gauravsingh9891 <gauravhacker8800@gmail.com>
2019-10-31 00:19:45 -0700

    Pg 28 qns = 2.4

---
+++ wap to read the value of a,b and c and display the value of x.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{
+	float x,a,b,c;
+	cout<<"\n Enter the value of a,b and c : ";
+	cin>>a>>b>>c;
+	x=a/(b-c);
+	cout<<"\n The new value of x is = "<<x;
+	getch();
+}
