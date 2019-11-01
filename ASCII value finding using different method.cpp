bec2d486ab65f6a29c07796448d876eacb52d9ab
gauravsingh9891 <gauravhacker8800@gmail.com>
2019-11-01 15:09:54 -0700

    Reverse of pg 28 qns no 2.3

---
+++ ASCII value finding using different method.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{
+	char ch;
+	cout<<"\n Enter the character : ";
+	cin>>ch;
+	cout<<"\n The ASCII value of "<<ch<<" "<<(int)ch;
+	getch();
+}
---
+++ reverse conversion of ASCII value into character.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{
+	int num;
+	cout<<"\n Enter the ASCII value that You want to character : ";
+	cin>>num;
+	cout<<"\n ASCII value "<<num<<" of character is "<<char(num);
+	getch();
+}
