c9e82ab289bb743019210f1a938b50f06dec3afb
gauravsingh9891 <gauravhacker8800@gmail.com>
2019-11-10 22:10:07 -0800

    Typing casting

---
+++ Typecasting.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{
+	int a=10;
+	float b=12.56;
+	cout<<"\n value of a : "<<a;
+	cout<<"\n value of b : "<<b;
+	cout<<"\n value of a in float = "<<float(a);
+	cout<<"\n Value of b in interger = "<<int(b);
+	getch();
+}
